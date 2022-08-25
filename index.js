35d31c56ec4880e19f3e369079e4e136b071d750
xiaodu1116 <Aynzy1116@163.com>
2022-08-25 23:36:54 +0800

    # 修改socket连接

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 const express = require('express')
 const http = require('http')
+const cors = require('cors')
 
 const app = express()
 
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // var http = require('http').Server(app);
 
-app.get('/', function(req, res){
+app.get('/list', (req, res) => {
   res.send('<h1>Hello world</h1>');
 });
 
-const PORT = 3001
+const PORT = process.abort.PORT || 3001
 
 //创建HTTP server
 const server = http.createServer(app);
 const socket = require("./src/websocket/socketio")
 process.io = socket.getSocket(server); //使用http协议建立socket
 //此处变成http listen
-// server.listen(3010);
+server.listen(3010);
 
 app.listen(PORT, () => {
-    console.log(`server running localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 })
--- src/websocket/socketio.js
+++ src/websocket/socketio.js
@@ -2,25 +2,25 @@ const socket = {}
 const socketio = require('socket.io')
 
 
-function getSocket(server) {
-    const io = socketio(server, {
-        cors: true,
-        maxHttpBufferSize: 3 * 1024 * 1024,
+function getSocket (server) {
+  const io = socketio(server, {
+    cors: true,
+    maxHttpBufferSize: 3 * 1024 * 1024,
+  })
+
+  io.on('connection', async socket => {
+    let timestamps = new Date().getTime()
+    console.log('连接成功')
+
+    socket.emit('message', {
+      msgType: '我是websocket',
+      status: '200',
+      time: timestamps
     })
 
-    io.on('connection', async socket => {
-        let timestamps = new Date().getTime()
-        console.log('12345')
+  })
 
-        socket.emit('message', {
-            msgType: '我是websocket',
-            status: '200',
-            time: timestamps
-        })
-
-    })
-
-    return io
+  return io
 }
 
 socket.getSocket = getSocket;

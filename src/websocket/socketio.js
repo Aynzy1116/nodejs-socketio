d012fd70c7fbbc01a68806db206fdb5fc6bf4d1a
Aynzy1116 <Aynzy1116@163.com>
2022-08-29 17:20:47 +0800

    # 简单看看

--- src/websocket/socketio.js
+++ src/websocket/socketio.js
@@ -9,11 +9,23 @@ function getSocket (server) {
   })
 
   io.on('connection', async socket => {
-    let timestamps = new Date().getTime()
+    // let timestamps = new Date().getTime() //获取时间
     console.log('连接成功')
+    // console.log('io.sockets.sockets',io.sockets.sockets) //可以看到当前在线人数
+    const socketID = socket.id
+    console.log('socket',socket.id);
+    //登录时建立一个username到socketID的映射表
+    socket.on('login',username=>{
+      socketHandler.saveUserSocketId(username,socketID)
+    })
 
     socket.on('message', (data) => {
       console.log('aaaaa', data)
+      // Idtoid.findOne({
+      //   username:data.receiveName
+      // }).then(rs=>{
+      //   console.log(rs)
+      // })
       socket.emit('message', data)
     })
 

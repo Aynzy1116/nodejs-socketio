114d925d1daac2d6d01151cda2a27f0699c1a234
Aynzy1116 <Aynzy1116@163.com>
2022-09-01 15:15:48 +0800

    # 向指定用户发送消息

--- src/websocket/socketio.js
+++ src/websocket/socketio.js
@@ -2,7 +2,7 @@ const socket = {}
 const socketio = require('socket.io')
 
 
-let userList = {}
+const userList = {}
 
 function getSocket (server) {
   const io = socketio(server, {
@@ -16,17 +16,20 @@ function getSocket (server) {
     // console.log('io.sockets.sockets',io.sockets.sockets) //可以看到当前在线人数
     const socketID = socket.id
     // console.log('socket', socket.id);
+    console.log('userList',userList)
 
     socket.on('login', user => {
-      // userList=Object.assign(userList,user.username,user)
-      userList[user.username] = user
+      userList[user.id] = socketID
       io.emit('userList',user)
       console.log('userList',userList)
     })
 
     socket.on('message', (data) => {
+      console.log('data',data)
+      console.log('list',userList)
+      console.log('userList',userList[data.to_Id])
       console.log('aaaaa', data)  
-      io.emit('message', data)
+      io.to(userList[data.to_Id]).emit('message', data)
     })
 
     socket.on('disconnect', () => {
